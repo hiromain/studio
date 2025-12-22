c8dfd62f724d291cb44a37fdb5f2e6cfe0401845
Romain Hittinger <romain.hittinger@gmail.com>
2025-12-22 21:38:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8164507608-7d4ae",
+  "appId": "1:421632347370:web:3084fad7da4ff8efce5b93",
+  "apiKey": "AIzaSyCkoOcwYAz-Zb9RtwHmzPniEFDur17IHMc",
+  "authDomain": "studio-8164507608-7d4ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "421632347370"
+};
